ca2d87887f3f26dabe704908ee8db9baa277fffd
Marci02 <nagymarci.0218@gmail.com>
2022-11-15 11:20:37 +0100

    Add project files.

---
+++ Beolvasos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Beolvasos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader be = new StreamReader("adatok.txt");
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    string[] filmek = be.ReadLine().Split(';');
+
+            //    string film = filmek[0];
+            //    int evszam = Convert.ToInt32(filmek[1]);
+
+            //    Console.WriteLine($"A(z) {i + 1}. legjobb film: {film} ({evszam})");
+            //}
+
+            int i = 0;
+
+            while (!be.EndOfStream)
+            {
+                string[] filmek = be.ReadLine().Split(';');
+
+                string film = filmek[0];
+                int evszam = Convert.ToInt32(filmek[1]);
+
+                Console.WriteLine($"A(z) {i+1}. legjobb film: {film.PadLeft(30)} ({evszam})");
+
+                i++;
+            }
+
+            be.Close();
+            Console.ReadKey();
+        }
+    }
+}
